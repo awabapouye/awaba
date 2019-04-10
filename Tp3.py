c04fdeaad2196f68d8392f2299b719c5deecbb5a
awabapouye <maamahawaa92@gmail.com>
2019-04-10 14:58:52 +0000

    first commit

---
+++ Tp3.py
@@ -0,0 +1,11 @@
+####Déclaration des variables
+n = input ("Saisir un nombre aléatoire n : ")
+n = int(n)
+
+if( n <= 9):
+    print("Le nombre trouvé est inférieur")
+    print("Bonne réponse!")
+
+else:
+    print("Le nombre trouvé est supérieur")
+    print("Mauvaise réponse!")
