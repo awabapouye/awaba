5183208f3956c8add1b062fd375eef898afa9bcb
awabapouye <maamahawaa92@gmail.com>
2019-04-10 10:32:40 +0000

    first commit

---
+++ Tp2.py
@@ -0,0 +1,8 @@
+##déclaration des variables
+n = input ("Tapez la valeur de n : ")
+n = int(n)
+r = n % 2
+if( r == 0):
+print("le nombre n tapé est pair")
+else:
+print("le nombre n tapé est impair")
